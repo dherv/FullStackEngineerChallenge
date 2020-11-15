2989f6a87ca59d9b1248bb577dcf809104eea766
dherv <damien.herv@gmail.com>
2020-11-15 10:24:55 +0900

    chore(server): add server database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'paypay_test_dh_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
